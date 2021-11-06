dd3ef729db4e5639234385db492284fda10fd788
LamHoang-01 <nhlam011019@gmail.com>
2021-11-06 10:39:00 +0700

    Add UI of main

--- src/components/main/Task.js
+++ src/components/main/Task.js
@@ -1,10 +1,42 @@
 import React from 'react';
 import "./task.scss";
+import { Card, Avatar, Progress, Tag } from 'antd';
+import { UserOutlined } from '@ant-design/icons';
 
-export default function Task() {
+export default function Task(props) {
   return (
-    <div>
-
+    <div className="task-card-container">
+        <Card 
+          hoverable
+          bordered={true} 
+          style={{ cursor: 'pointer', borderRadius: '5px'}} 
+          bodyStyle={{ padding: '4%', height: '133px' }}
+          onClick={() => alert("Hello from here")}
+        >
+          <div style={{ display: 'flex' }}>
+            <p className="title">{props.name}</p>
+            <Avatar.Group style={{ marginLeft: 'auto', marginRight: 0 }}>
+              <Avatar src="https://joeschmoe.io/api/v1/random" />
+              <Avatar style={{ backgroundColor: '#f56a00' }}>K</Avatar>
+              <Avatar style={{ backgroundColor: '#1890ff' }} icon={<UserOutlined />} />
+            </Avatar.Group>
+          </div>
+          <div style={{ marginTop: '-15px'}}>
+            <Progress 
+              percent={props.progression} 
+              status="active" 
+              showInfo={false} 
+              strokeColor="#805454"
+              trailColor="#c4c4c4"
+              style={{ width: '65%' }}
+            />
+            <p className="date">{props.deadline}</p>
+          </div>
+          <div style={{ marginTop: '-20px', float: 'right'}}>
+            <Tag color="#f0f8ef" style={{ color: '#60B158', fontFamily: 'arial', fontWeight: 'bold', fontSize: '14px' }}>IOS APP</Tag>
+            <Tag color="#E6F2FF" style={{ color: '#007AFF', fontFamily: 'arial', fontWeight: 'bold', fontSize: '14px' }}>UX/UI</Tag>
+          </div> 
+        </Card>
     </div>
   )
 }
