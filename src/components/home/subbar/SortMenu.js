3fad23433e07706357cf1e39b5ddc41df92c58e2
nguyenhuuphuc.cse <nguyenhuuphuc.cse@gmail.com>
2021-11-20 00:13:51 +0700

    add sort: single criteria

--- src/components/home/subbar/SortMenu.js
+++ src/components/home/subbar/SortMenu.js
@@ -1,75 +1,79 @@
-import { useState } from "react";
+import { useState, useContext } from "react";
 import { Menu, Select } from "antd";
 import { PlusOutlined, CloseOutlined } from "@ant-design/icons";
+import { ViewContext, Field, Sort } from "../../../context/ViewProvider";
+
 const { Option } = Select;
 
-// TODO: 
-const defaultSortRules = [
-  { id: "deadline", name: "Due date", value: null },
-  { id: "priority", name: "Priority", value: null },
-  { id: "progress", name: "Progress", value: null },
-];
-
-const OptionList = (
-  <>
-    {defaultSortRules.map((option) => (
-      <Option key={option.id} value={option.id}>
-        {option.name}
-      </Option>
-    ))}
-  </>
-);
 
 export default function SortMenu() {
-  const [sortRules, setSortRules] = useState(defaultSortRules);
+  const {sortOptions, setSortOptions} = useContext(ViewContext);
+  const [allowedOptions, setAllowedOptions] = useState([
+    { id: Field.NAME, value: Sort.ASC },
+    { id: Field.DEADLINE, value: Sort.ASC },
+    { id: Field.PRIORITY, value: Sort.ASC },
+    { id: Field.PROGRESS, value: Sort.ASC },
+  ]);
+  const [selectedOptions, setSelectedOptions] = [sortOptions, setSortOptions];
+  const [flush, setFlush] = useState(false);
 
-  const handleChange = (victimId, value) => {
-    const temp = [...sortRules].map((rule) => {
-      if (rule.id === victimId) rule.value = value;
-      return rule;
-    });
-    console.log(sortRules.map((f) => (f.value ? f.name + ": " + f.value : null)));
-    setSortRules(temp);
+  console.log(selectedOptions.map((f) => (f.id + ": " + f.value )));
+  const handleChange = (victimId, candidate) => {
+    const ref1 = selectedOptions.find(f => f.id === victimId);
+    const ref2 = allowedOptions.find(f => f.id === candidate);
+    const temp = ref1.id;
+    ref1.id = ref2.id;
+    ref2.id = temp;
+    setSelectedOptions(selectedOptions);
+    setAllowedOptions(allowedOptions);
+    setFlush(!flush);
   };
+  const handleChangeValue = (rule, newValue) => {
+    rule.value = newValue;
+    setAllowedOptions(allowedOptions);
+    setSelectedOptions([...selectedOptions]);
+    setFlush(!flush);
+  }
   const handleRemoveRule = (victimId) => {
-    const temp = [...sortRules].map((rule) => {
-      if (rule.id === victimId) rule.value = null;
-      return rule;
-    });
-    setSortRules(temp);
+    const pos = selectedOptions.findIndex(f => f.id === victimId);
+    const victim = selectedOptions.splice(pos, 1)[0];
+    setSelectedOptions(selectedOptions);
+    setAllowedOptions([...allowedOptions, victim]);
   };
   const handleAddRule = () => {
-    const temp = [...sortRules];
-    for (let i = 0; i < temp.length; i++) {
-      if (temp[i].value === null) {
-        temp[i].value = "ascending";
-        break;
-      }
+    if (!allowedOptions || allowedOptions.length === 0) {
+      return;
     }
-    setSortRules(temp);
+    const victim = allowedOptions.pop();
+    setAllowedOptions(allowedOptions);
+    setSelectedOptions([...selectedOptions, victim]);
   };
-
+  
   return (
     <Menu className="my-menu">
-      {sortRules.map((sortRule) =>
+      {selectedOptions.map((sortRule) =>
         sortRule.value === null ? null : (
           <Menu.Item key={sortRule.id}>
             <div className="row">
               <div className="col">
                 <Select
-                  defaultValue={sortRule.name}
+                  defaultValue={sortRule.id}
                   style={{ width: "7rem" }}
-                  // onChange={(e) => handleChange(sortRule.id, value)} // TODO: handle change
+                  onChange={(candidate) => handleChange(sortRule.id, candidate)}
                 >
-                  {OptionList}
+                {allowedOptions.map((option) => (
+                  <Option label={option.id} key={option.id} value={option.id}>
+                    {option.id}
+                  </Option>
+                ))}
                 </Select>{' '}
                 <Select
                   defaultValue={sortRule.value}
                   style={{ width: "8rem" }}
-                  onChange={(value) => handleChange(sortRule.id, value)}
+                  onChange={(value) => handleChangeValue(sortRule, value)} // TODO
                 >
-                  <Option value="ascending">Ascending</Option>
-                  <Option value="descending">Descending</Option>
+                  <Option value={Sort.ASC}>{Sort.ASC}</Option>
+                  <Option value={Sort.DESC}>{Sort.DESC}</Option>
                 </Select>
               </div>
               <div className="col">
--- src/context/ViewProvider.js
+++ src/context/ViewProvider.js
@@ -1,11 +1,10 @@
-import React, { useState } from 'react';
+import React, { useState, useContext, useMemo, useEffect } from 'react';
+import { AppContext } from '../context/AppProvider';
+import { cmpNAME, cmpPRIORITY, cmpDEADLINE, cmpPROGRESS } from './customFunction';
 
 export const ViewContext = React.createContext();
 
-/**
- * Enum type used for view context
- */
-export const Field = { // sth
+const _Field = { // collectionParam in firebase
   NAME: 'name',
   CREATED_AT: 'createdAt',
   CREATED_BY: 'createdBy',
@@ -14,18 +13,61 @@ export const Field = { // sth
   PROGRESS: 'progression',
   MEMBER: 'memberIdList',
 }
+export const Field = { // Used for display
+  NAME: 'Title',
+  CREATED_AT: 'Created at',
+  CREATED_BY: 'Created by',
+  MEMBER: 'Assignees',
+  DEADLINE: 'Due date',
+  PRIORITY: 'Priority',
+  PROGRESS: 'Progress',
+}
+export const Sort = {
+  ASC: 'Ascending',
+  DESC: 'Descending',
+}
+Object.freeze(_Field);
 Object.freeze(Field);
+Object.freeze(Sort);
+
+function applySortOption (tasks, sortOption) {
+  const res = [...tasks];
+  res.sort(FuncFactory(sortOption.id, sortOption.value));
+  return res;
+}
+
+function FuncFactory (aField, aSort) {
+  const func = (task1, task2) => {
+    const factor = aSort === Sort.ASC ? 1 : -1;
+    switch (aField) {
+      case Field.NAME:
+        return factor * cmpNAME(task1[_Field.NAME], task2[_Field.NAME]) || 0;
+      case Field.PRIORITY:
+        return factor * cmpPRIORITY(task1[_Field.PRIORITY], task2[_Field.PRIORITY]) || 0;
+      case Field.DEADLINE:
+        return factor * cmpDEADLINE(task1[_Field.DEADLINE], task2[_Field.DEADLINE]) || 0;
+      case Field.PROGRESS:
+        return factor * cmpPROGRESS(task1[_Field.PROGRESS], task2[_Field.PROGRESS]) || 0;
+      default:
+        throw "Not yet handled";
+    }
+  }
+  // console.log("I have a func now");
+  return func;
+}
+
 
 export default function ViewProvider({ children }) {
+  // To Show/hide stuff in task
   const defaultState = () => {
     return [
-      { visible: true, fieldName: Field.NAME, displayName: "Name"},
-      { visible: true, fieldName: Field.CREATED_AT, displayName: "Created at"},
-      { visible: true, fieldName: Field.CREATED_BY, displayName: "Created by"},
-      { visible: true, fieldName: Field.DEADLINE, displayName: "Due date"},
-      { visible: true, fieldName: Field.PRIORITY, displayName: "Priority"},
-      { visible: true, fieldName: Field.PROGRESS, displayName: "Progress"},
-      { visible: true, fieldName: Field.MEMBER, displayName: "Assignees"},
+      { visible: true, fieldName: Field.NAME },
+      { visible: true, fieldName: Field.CREATED_AT },
+      { visible: true, fieldName: Field.CREATED_BY },
+      { visible: true, fieldName: Field.DEADLINE },
+      { visible: true, fieldName: Field.PRIORITY },
+      { visible: true, fieldName: Field.PROGRESS },
+      { visible: true, fieldName: Field.MEMBER },
     ];
   }
   const [state, setState] = useState(defaultState());
@@ -47,12 +89,28 @@ export default function ViewProvider({ children }) {
     return _field(field).visible;
   }
 
+  // To sort task in workspace
+  // { id: Field.DEADLINE, value: Sort.ASC },
+  // { id: Field.PRIORITY, value: Sort.ASC },
+  // { id: Field.PROGRESS, value: Sort.ASC },
+  const [sortOptions, setSortOptions] = useState([]);
+  const {tasks} = useContext(AppContext); 
+  const sortedTasks = useMemo(() => {
+    let taskList = [...tasks];
+    for (let i = 0; i < sortOptions.length; i++) {
+      taskList = applySortOption(taskList, sortOptions[i]);
+    }
+    return taskList;
+  }, [tasks, sortOptions]);
+  
   return (
     <ViewContext.Provider
       value={{ 
-        state,
         setFieldVisible,
         isFieldVisible,
+        sortOptions,
+        setSortOptions,
+        sortedTasks,
     }}>
       {children}
     </ViewContext.Provider>
